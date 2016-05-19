9662fefc631c310b5907fe880039ecc309de0ce2
Jacob Clarke <jacob@bigfish.tv>
2016-05-19 16:10:45 +1000

    Put dist in demo folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 		waterify: './src/index.js',
 	},
 	output: {
-		path: path.join(__dirname, 'dist'),
+		path: path.join(__dirname, 'demo', 'dist'),
 		filename: '[name].js',
 	},
 	plugins: [new webpack.NoErrorsPlugin()],
